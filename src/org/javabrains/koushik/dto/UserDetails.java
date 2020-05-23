0fa55c73cea9fabd53e6d2de289bf020c231325d
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-05-23 21:48:14 +0530

    Hibernate Tutorial 05 - More Annotations

--- src/org/javabrains/koushik/dto/UserDetails.java
+++ src/org/javabrains/koushik/dto/UserDetails.java
@@ -1,17 +1,32 @@
 package org.javabrains.koushik.dto;
 
+import java.util.Date;
+
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
-@Entity(name = "USER_DETAILS")
+@Entity
+@Table(name = "USER_DETAILS")
 public class UserDetails {
-	
-	private int userId;
-	private String userName;
-	
 	@Id
-	@Column(name = "USER_ID")
+	private int userId;
+//	@Basic
+	@Transient
+	private String userName;
+	@Temporal(TemporalType.DATE)
+	private Date joinedDate;
+	private String address;
+	@Lob
+	private String description;
+	
+	
 	public int getUserId() {
 		return userId;
 	}
@@ -24,6 +39,24 @@ public class UserDetails {
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
 	}
 	
 
--- src/org/koushik/hibernate/HibernateTest.java
+++ src/org/koushik/hibernate/HibernateTest.java
@@ -1,5 +1,7 @@
 package org.koushik.hibernate;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -14,6 +16,10 @@ public class HibernateTest {
 		UserDetails user = new UserDetails();
 		user.setUserId(1);
 		user.setUserName("first name");
+		user.setJoinedDate(new Date());
+		user.setAddress("First user's address ");
+		user.setDescription("users description goes here ");
+		
 				
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
