1037865774b40b977f953b206a1c05ee61fe855b
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-05-23 17:15:49 +0530

    first commit

---
+++ src/org/koushik/hibernate/HibernateTest.java
@@ -0,0 +1,27 @@
+package org.koushik.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.javabrains.koushik.dto.UserDetails;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		
+		UserDetails user = new UserDetails();
+		user.setUserId(2);
+		user.setUserName("first name");
+				
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+		
+
+	}
+
+}
