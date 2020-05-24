72fce516e42d427a53f828a0b15f36745f1c2494
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-05-24 23:18:15 +0530

    Hibernate Tutorial 14 - One To Many Mapping

--- src/org/javabrains/koushik/dto/UserDetails.java
+++ src/org/javabrains/koushik/dto/UserDetails.java
@@ -12,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -26,15 +27,18 @@ public class UserDetails {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int userId;
 	private String userName;
-	@OneToOne
-	@JoinColumn(name = "VEHICLE_ID")
-	private Vehicle vehicle;
+	
+	@OneToMany
+	@JoinTable( name = "USER_VEHICLE" , joinColumns = @JoinColumn(name = "USER_ID"),
+				inverseJoinColumns = @JoinColumn(name = "VEHICLE_ID")
+			)
+	private Collection<Vehicle> vehicle = new ArrayList<Vehicle>();
 	
 	
-	public Vehicle getVehicle() {
+	public Collection<Vehicle> getVehicle() {
 		return vehicle;
 	}
-	public void setVehicle(Vehicle vehicle) {
+	public void setVehicle(Collection<Vehicle> vehicle) {
 		this.vehicle = vehicle;
 	}
 	public int getUserId() {
--- src/org/koushik/hibernate/HibernateTest.java
+++ src/org/koushik/hibernate/HibernateTest.java
@@ -21,13 +21,19 @@ public class HibernateTest {
 		Vehicle vehicle = new Vehicle();
 		vehicle.setVehicleName("car");
 		
-		user.setVehicle(vehicle);
+		Vehicle vehicle2 = new Vehicle();
+		vehicle2.setVehicleName("Jeep");
+		
+		user.getVehicle().add(vehicle);
+		user.getVehicle().add(vehicle2);
+		
 				
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(user);
 		session.save(vehicle);
+		session.save(vehicle2);
 		session.getTransaction().commit();
 		session.close();
 		
