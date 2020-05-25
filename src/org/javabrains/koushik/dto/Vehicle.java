b790710c2d7d8f97b1cc78c606366b2e2509e7ab
Abhi16085002 <abhishek.kumar.eee16@itbhu.ac.in>
2020-05-25 17:25:30 +0530

    Hibernate Tutorial 18 - Implementing Inheritance - Single Table Strategy

--- src/org/javabrains/koushik/dto/Vehicle.java
+++ src/org/javabrains/koushik/dto/Vehicle.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
@@ -16,27 +20,23 @@ import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(
+		name = "VEHICLE_TYPE",
+		discriminatorType = DiscriminatorType.STRING
+		)
 public class Vehicle {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int vehicleId;
 	private String vehicleName;
-	
-//	@ManyToOne
-//	@NotFound(action = NotFoundAction.IGNORE)
-//	private UserDetails user;
 
 	
 	public int getVehicleId() {
 		return vehicleId;
 	}
-//	public UserDetails getUser() {
-//		return user;
-//	}
-//	public void setUser(UserDetails user) {
-//		this.user = user;
-//	}
+
 	public void setVehicleId(int vehicleId) {
 		this.vehicleId = vehicleId;
 	}
